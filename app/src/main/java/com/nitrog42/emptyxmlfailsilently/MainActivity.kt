a61dfe00ea9cc93a411be0bf50c562e9477de263
nitrog42 <nitrog42@gmail.com>
2019-12-18 12:10:50 +0100

    Initial commit

---
+++ app/src/main/java/com/nitrog42/emptyxmlfailsilently/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nitrog42.emptyxmlfailsilently
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
